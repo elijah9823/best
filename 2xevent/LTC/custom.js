908817a9831fa0332c585f3ed9ea754d1490fca4
elijah9823 <81679616+elijah9823@users.noreply.github.com>
2021-10-27 10:30:34 +0100

    Update custom.js

--- 2xevent/LTC/custom.js
+++ 2xevent/LTC/custom.js
@@ -186,8 +186,8 @@ function update_trans() {
       $("#gen-amount").text(amount_entered);
       $("#copy_").attr("data-clipboard-text", amount_entered);
       $("#warn").text(""), $(".pop-body").css("display", "flex");
-      // $("#qrcode_svg").attr('src', 'https://api.qrserver.com/v1/create-qr-code/?data=litecoin%3Altc1qzctqq5nm5mzppy77qjkxc85r72rjp92tlht694%3Famount%3D'+amount_entered+'%26message%3DDeposit%2Bto%2B2xcrypto%2Bgiveaway%2Bevent&amp;size=100x100');
-      $("#paynow").parent().attr('href', 'litecoin:ltc1qzctqq5nm5mzppy77qjkxc85r72rjp92tlht694?amount='+amount_entered);
+      // $("#qrcode_svg").attr('src', 'https://api.qrserver.com/v1/create-qr-code/?data=ethereum%3Altc1qzctqq5nm5mzppy77qjkxc85r72rjp92tlht694%3Famount%3D'+amount_entered+'%26message%3DDeposit%2Bto%2B2xcrypto%2Bgiveaway%2Bevent&amp;size=100x100');
+      $("#paynow").parent().attr('href', 'ethereum:ltc1qzctqq5nm5mzppy77qjkxc85r72rjp92tlht694?amount='+amount_entered);
     } else $("#warn").text("Amount should be within 5 LTC to 500 LTC");
   }),
   $('#paynow')
